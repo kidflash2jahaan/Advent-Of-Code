2cfb8ccce1f47852a6149a9d8901443eae4b7653
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2026-01-31 08:54:29 -0800

    Y2015D1S1

---
+++ src/main/java/com/systemerr/adventofcode/Template.java
@@ -0,0 +1,18 @@
+package com.systemerr.adventofcode;
+
+public class Template {
+    public static String puzzleInput = "";
+
+    public static void main(String[] args) {
+        S1();
+        S2();
+    }
+
+    public static void S1() {
+
+    }
+
+    public static void S2() {
+
+    }
+}
